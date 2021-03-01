8e94dd14acdbe2206deb9faf88e11ae3e1ea3674
Martin Ingram <dev.bloke@devbloke.me.uk>
2021-03-01 15:45:05 +0000

    Added range examples.

--- python/simple/loops.py
+++ python/simple/loops.py
@@ -6,3 +6,20 @@ while num < 10:
 my_list = ["a", "b", "c"]
 for letter in my_list:
     print(letter)
+
+for i in range(len(my_list)):
+    print(my_list[i])
+
+for i in range(2, len(my_list)):
+    print(my_list[i])
+
+for i in range(len(my_list) - 1, -1 , -1):
+    print(my_list[i])
+
+list_of_tuples = [(1, 2), (3, 4), (5, 6)]
+for x, y in list_of_tuples:
+    sum = x + y
+    print(sum)
+
+for i, value in enumerate(my_list):
+    print("my_list[{0}] = {1}".format(i, value))
