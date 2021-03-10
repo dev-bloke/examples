3726fb0010a7a6ccd016871126e01bfac375ebf6
Martin Ingram <dev.bloke@devbloke.me.uk>
2021-03-10 15:15:12 +0000

    Added an example function that returns a tuple (pair).

--- kotlin/kotlin-basics/src/main/kotlin/com/meridal/examples/kotlinbasics/ExampleRange.kt
+++ kotlin/kotlin-basics/src/main/kotlin/com/meridal/examples/kotlinbasics/ExampleRange.kt
@@ -12,4 +12,19 @@ object ExampleRange {
             "Not between 1 and 20"
         }
     }
+
+    fun getValuesBasedOnRange(number: Int) = when (number) {
+        in 1..10 -> {
+            Pair("positive", "Between 1 and 10")
+        }
+        in 11..20 -> {
+            Pair("positive", "Between 11 and 20")
+        }
+        0 -> {
+            Pair("zero", "Zero")
+        }
+        else -> {
+            Pair("unknown", "Not between 0 and 20")
+        }
+    }
 }
--- kotlin/kotlin-basics/src/test/kotlin/com/meridal/examples/kotlinbasics/ExampleRangeSpec.kt
+++ kotlin/kotlin-basics/src/test/kotlin/com/meridal/examples/kotlinbasics/ExampleRangeSpec.kt
@@ -26,5 +26,29 @@ class ExampleRangeSpec: Spek({
         it ("should detect that zero is not between one and twenty") {
             assert(ExampleRange.getValueBasedOnRange(0) == "Not between 1 and 20")
         }
+
+        it ("should detect that a number between one and ten is positive") {
+            val (type, message) = ExampleRange.getValuesBasedOnRange(5)
+            assert(type == "positive")
+            assert(message == "Between 1 and 10")
+        }
+
+        it ("should detect a number between eleven and twenty is positive") {
+            val (type, message) = ExampleRange.getValuesBasedOnRange(16)
+            assert(type == "positive")
+            assert(message == "Between 11 and 20")
+        }
+
+        it ("should not be able to identify the type of a number that is not between zero and twenty") {
+            val (type, message) = ExampleRange.getValuesBasedOnRange(27)
+            assert(type == "unknown")
+            assert(message == "Not between 0 and 20")
+        }
+
+        it ("should detect that zero is neither positive or negative") {
+            val (type, message) = ExampleRange.getValuesBasedOnRange(0)
+            assert(type == "zero")
+            assert(message == "Zero")
+        }
     }
 })
