6ae6d85ffd923499f31e64653a351e164c3367b6
Martin Ingram <dev.bloke@devbloke.me.uk>
2021-03-10 15:18:49 +0000

    Removed unnecessary braces.

--- kotlin/kotlin-basics/src/main/kotlin/com/meridal/examples/kotlinbasics/ExampleRange.kt
+++ kotlin/kotlin-basics/src/main/kotlin/com/meridal/examples/kotlinbasics/ExampleRange.kt
@@ -2,29 +2,15 @@ package com.meridal.examples.kotlinbasics
 
 object ExampleRange {
     fun getValueBasedOnRange(number: Int) = when (number) {
-        in 1..10 -> {
-            "Between 1 and 10"
-        }
-        in 11..20 -> {
-            "Between 11 and 20"
-        }
-        else -> {
-            "Not between 1 and 20"
-        }
+        in 1..10  -> "Between 1 and 10"
+        in 11..20 -> "Between 11 and 20"
+        else      -> "Not between 1 and 20"
     }
 
     fun getValuesBasedOnRange(number: Int) = when (number) {
-        in 1..10 -> {
-            Pair("positive", "Between 1 and 10")
-        }
-        in 11..20 -> {
-            Pair("positive", "Between 11 and 20")
-        }
-        0 -> {
-            Pair("zero", "Zero")
-        }
-        else -> {
-            Pair("unknown", "Not between 0 and 20")
-        }
+        in 1..10  -> Pair("positive", "Between 1 and 10")
+        in 11..20 -> Pair("positive", "Between 11 and 20")
+        0         -> Pair("zero", "Zero")
+        else      -> Pair("unknown", "Not between 0 and 20")
     }
 }
