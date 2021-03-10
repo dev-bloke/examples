3a2beae0f3610490018a5058978d5502777aedf3
Martin Ingram <dev.bloke@devbloke.me.uk>
2021-03-10 11:55:29 +0000

    Refactoring test configuration into common conventions.

--- kotlin/kotlin-basics/build.gradle.kts
+++ kotlin/kotlin-basics/build.gradle.kts
@@ -1,16 +1,3 @@
 plugins {
     id("com.meridal.examples.kotlin-library-conventions")
 }
-
-dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-    testCompileOnly("junit:junit:4.13.2")
-    testRuntimeOnly("org.junit.vintage:junit-vintage-engine")
-}
-
-tasks.test {
-    useJUnitPlatform {
-        includeEngines("junit-vintage")
-    }
-}
