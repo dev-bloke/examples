fbbf58bd11c518df5fbd921f2480d4807983b951
Martin Ingram <dev.bloke@devbloke.me.uk>
2021-03-10 14:02:30 +0000

    Example data class and associated test spec.

---
+++ kotlin/kotlin-basics/src/main/kotlin/com/meridal/examples/kotlinbasics/ExampleDataClass.kt
@@ -0,0 +1,5 @@
+package com.meridal.examples.kotlinbasics
+
+data class ExampleDataClass(val key: String, val name: String) {
+    var description: String? = null
+}
---
+++ kotlin/kotlin-basics/src/test/kotlin/com/meridal/examples/kotlinbasics/ExampleDataClassSpec.kt
@@ -0,0 +1,37 @@
+package com.meridal.examples.kotlinbasics
+
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+
+private const val KEY = "key"
+private const val NAME = "name"
+private const val DESCRIPTION = "description"
+private const val OTHER_KEY = "other key"
+
+class ExampleDataClassSpec: Spek({
+
+    val exampleData = ExampleDataClass(KEY, NAME)
+
+    describe ("An example data class") {
+
+        it ("should be comparable based on its constructor parameters") {
+            val other = ExampleDataClass(KEY, NAME)
+            assert(other == exampleData)
+            val notTheSame = ExampleDataClass(OTHER_KEY, NAME)
+            assert(notTheSame != exampleData)
+        }
+
+        it ("should be comparable based on its hashcode") {
+            val other = ExampleDataClass(KEY, NAME)
+            assert(other.hashCode() == exampleData.hashCode())
+            val notTheSame = ExampleDataClass(OTHER_KEY, NAME)
+            assert(notTheSame.hashCode() != exampleData.hashCode())
+        }
+
+        it ("should not be comparable based on the description") {
+            val other = ExampleDataClass(KEY, NAME)
+            other.description = DESCRIPTION
+            assert(other == exampleData)
+        }
+    }
+})
