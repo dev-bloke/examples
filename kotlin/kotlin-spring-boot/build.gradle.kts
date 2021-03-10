32b73d77a5993cb398bb054aa25712d4c242bd44
Martin Ingram <dev.bloke@devbloke.me.uk>
2021-03-10 17:20:07 +0000

    Initial Kotlin Spring Boot example.

---
+++ kotlin/kotlin-spring-boot/build.gradle.kts
@@ -0,0 +1,29 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.4.3"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	kotlin("jvm")
+	kotlin("plugin.spring") version "1.4.30"
+}
+
+group = "com.meridal.examples"
+version = "1.0-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "11"
+	}
+}
+
--- kotlin/settings.gradle.kts
+++ kotlin/settings.gradle.kts
@@ -1,2 +1,3 @@
 rootProject.name = "kotlin-parent"
 include("kotlin-basics")
+include("kotlin-spring-boot")
