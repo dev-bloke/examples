8f512f0521ae20807ac358fcde5936a01c04db16
Martin Ingram <dev.bloke@devbloke.me.uk>
2021-03-18 15:50:09 +0000

    Initial Spring Boot Simple project.

---
+++ java/spring-boot-simple/README.md
@@ -0,0 +1,6 @@
+# Spring Boot Simple
+
+A basic Spring Boot REST microservice written in Java.
+
+This example uses an embedded H2 database to keep the runtime and testing simple.
+
---
+++ java/spring-boot-simple/src/test/java/com/meridal/examples/springboot/SpringBootSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meridal.examples.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
