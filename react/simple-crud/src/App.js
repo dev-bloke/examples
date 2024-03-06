6b24e231a5690f8d071e88437649facf5e7746c9
Martin Ingram <martin@martiningram.uk>
2024-03-06 16:22:24 +0000

    A simple CRUD application.

---
+++ react/simple-crud/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Create from './components/create'
+import Read from './components/read';
+import Update from './components/update';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+function App() {
+    return (
+        <Router>
+            <div className="main">
+                <h2 className="main-header">React Crud Operations</h2>
+                <div>
+                    <Routes>
+                        <Route path='/create' element={<Create />} />
+                        <Route path='/' element={<Read />} />
+                        <Route path='/update' element={<Update />} />
+                    </Routes>
+                </div>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
