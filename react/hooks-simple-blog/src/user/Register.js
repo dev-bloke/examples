2c50c42e56c1b468c6d39331acb0b700cefae03c
Martin Ingram <martin@martiningram.uk>
2024-03-13 18:47:03 +0000

    Register user uses the REST API.

--- react/hooks-simple-blog/src/user/Register.js
+++ react/hooks-simple-blog/src/user/Register.js
@@ -1,4 +1,5 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
+import { useResource } from "react-request-hook";
 import { StateContext } from "../contexts";
 
 export default function Register() {
@@ -7,6 +8,18 @@ export default function Register() {
     const [ password, setPassword ] = useState('')
     const [ repeatPassword, setRepeatPassword ] = useState('')
     const { dispatch } = useContext(StateContext)
+
+    const [ user, register ] = useResource((username, password) => ({
+        url: '/user',
+        method: 'post',
+        data: { username, password }
+    }))
+
+    useEffect(() => {
+        if (user && user.data) {
+            dispatch({ type: 'REGISTER', username: user.data.username })
+        }
+    }, [user])
 
     function handleUsername (event) {
         setUsername(event.target.value)
@@ -21,7 +34,7 @@ export default function Register() {
     }
 
     return (
-        <form onSubmit={e => { e.preventDefault(); dispatch({type: "REGISTER", username}) }}>
+        <form onSubmit={e => { e.preventDefault(); register(username, password) }}>
             <label htmlFor="register-username">username:</label>
             <input type="text" name="register-username" id="register-username"
                 value={username} onChange={handleUsername} />
